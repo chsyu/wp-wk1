65d09b5ede7bc39cbeb837ebefe9bdee933428d0
Chi-Shan Yu <chsyu@tea.ntue.edu.tw>
2018-04-27 01:29:42 +0800

    my theme

---
+++ wp-content/themes/wpwk1/single-event.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<h1 class="event-title">Event Lists</h1>
+<?php
+        while(have_posts()){
+            the_post(); ?>
+            <h2><?php the_title()  ?></h2>
+            <p><?php the_content()  ?></p>
+            <hr>
+       <?php }
+    ?>
+<?php get_footer(); ?>
